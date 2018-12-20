1b261a66d1d4bdd68803f2bf492f00c4119f1526
daudras <44780746+daudras@users.noreply.github.com>
2018-12-20 17:22:53 +0100

    Add files via upload

---
+++ exCellules.py
@@ -0,0 +1,17 @@
+﻿#exemple de matrice de cellules
+#nb de colonnes entre 1 et 10
+#nb de ligne : li
+#les probabilités sont ici uniformes,
+#il faudra les pondérer par la suite.
+
+from random import randint
+def exCellules(li):
+    cellules=[]
+    ligne=[]
+    for i in range(li):
+        nbCol=randint(1,10)
+        for j in range(nbCol):
+            ligne.append([randint(1,3),randint(0,1),randint(0,1),
+            randint(-3,3),randint(0,3),randint(0,7)])
+        cellules.append(ligne)
+    return cellules
